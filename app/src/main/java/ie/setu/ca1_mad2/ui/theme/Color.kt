b1f42df7edb1fa20d19684f9609f46e42ce756cf
CiaranHickey5 <ciaran.hickey99@gmail.com>
2025-03-05 17:03:58 +0000

    Update theme and colours

--- app/src/main/java/ie/setu/ca1_mad2/ui/theme/Color.kt
+++ app/src/main/java/ie/setu/ca1_mad2/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BluePrimary = Color(0xFF2196F3)
+val BlueDark = Color(0xFF1976D2)
+
+val YellowSecondary = Color(0xFFFFEB3B)
+val YellowDark = Color(0xFFFBC02D)
