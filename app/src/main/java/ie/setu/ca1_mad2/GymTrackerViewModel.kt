46430910a0515f9e9152e7b92367f7ce127b30f6
CiaranHickey5 <ciaran.hickey99@gmail.com>
2025-03-06 00:38:47 +0000

    Edit ViewModel to add workouts list and add exercises to workouts

--- app/src/main/java/ie/setu/ca1_mad2/GymTrackerViewModel.kt
+++ app/src/main/java/ie/setu/ca1_mad2/GymTrackerViewModel.kt
@@ -3,6 +3,7 @@ package ie.setu.ca1_mad2
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import ie.setu.ca1_mad2.model.Exercise
+import ie.setu.ca1_mad2.model.Workout
 
 class GymTrackerViewModel : ViewModel() {
 
@@ -15,6 +16,23 @@ class GymTrackerViewModel : ViewModel() {
             _exercises.add(
                 Exercise(name = name, muscleGroup = muscleGroup)
             )
+        }
+    }
+
+    private val _workouts = mutableStateListOf<Workout>()
+    val workouts: List<Workout> get() = _workouts
+
+    fun addWorkout(name: String, description: String) {
+        if (name.isNotBlank()) {
+            _workouts.add(Workout(name = name, description = description))
+        }
+    }
+
+    // Add an exercise to a specific workout
+    fun addExerciseToWorkout(workoutId: String, exerciseName: String, exerciseMuscleGroup: String) {
+        val workout = _workouts.find { it.id == workoutId } ?: return
+        if (exerciseName.isNotBlank()) {
+            workout.exercises.add(Exercise(name = exerciseName, muscleGroup = exerciseMuscleGroup))
         }
     }
 }
