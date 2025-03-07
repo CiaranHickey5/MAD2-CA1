e162e892d7d84bb3885fe9646a3ee9205783f6b1
CiaranHickey5 <ciaran.hickey99@gmail.com>
2025-03-07 00:12:55 +0000

    Add BottomAppBarProvider for route navigation

---
+++ app/src/main/java/ie/setu/ca1_mad2/ui/general/BottomAppBarProvider.kt
@@ -0,0 +1,44 @@
+package ie.setu.ca1_mad2.ui.general
+
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import ie.setu.ca1_mad2.navigation.AppRoutes
+
+data class NavBarItem(
+    val route: String,
+    val label: String
+)
+
+// Function that returns main nav items
+fun navBarItems() = listOf(
+    NavBarItem(AppRoutes.HOME, "Home"),
+    NavBarItem(AppRoutes.ADD_WORKOUT, "Add WO"),
+    NavBarItem(AppRoutes.LIST_WORKOUTS, "List WO")
+)
+
+@Composable
+fun BottomAppBarProvider(navController: NavController) {
+    NavigationBar {
+        val backStackEntry = navController.currentBackStackEntryAsState()
+        val currentRoute = backStackEntry.value?.destination?.route
+
+        navBarItems().forEach { item ->
+            NavigationBarItem(
+                selected = (item.route == currentRoute),
+                onClick = {
+                    // Pop up to avoid stacking multiple copies
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                },
+                label = { Text(item.label) },
+                icon = {}
+            )
+        }
+    }
+}
